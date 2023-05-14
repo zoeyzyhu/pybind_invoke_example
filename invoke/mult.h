19a5f23946391186016340de529208f2a9204c75
zoey.zyhu <zoey.zyhu@gmail.com>
2023-05-14 16:09:30 +0200

    delete the original code in subfolder

--- invoke/mult.h
+++
@@ -1,6 +0,0 @@
-#ifndef MULT_H
-#define MULT_H
-
-float mult(int int_param, float float_param);
-
-#endif
--- invoke/pybind11_wrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include <pybind11/pybind11.h>
-#include "mult.h"
-
-PYBIND11_MODULE(pybind11_toy, module) {
-    module.doc() = "pybind11 toy example"; // Optional
-    module.def("cpp_function", &mult, 
-		    "A function multiplying two numbers");
-}
